5c6ea86227af0f368619a216f0a7a33c2b5bf3b1
Sachin Pareek <49728275+Pareeksachin@users.noreply.github.com>
2024-05-20 17:31:24 +0530

    Updated ggplot2

--- ggplot2.R
+++ ggplot2.R
@@ -42,6 +42,108 @@ install.packages('ggplot2movies')
 library(ggplot2movies)
 movies
 
+
+
+################################ Histogram #################################################
 ## data & Aesthetics
-ggplot(movies,aes())
+pl <- ggplot(movies,aes(x = rating))
+
+# geometry layer
+pl2 <- pl+geom_histogram(binwidth = 0.1,color='red',fill='pink',alpha=0.4)
+#or
+pl2 <- pl+geom_histogram(binwidth = 0.1,aes(fill = ..count..)) # aesthetic colour
+# alpha is for transparancy
+pl3 <- pl2 + xlab("Movie Rating")+ylab('Count')
+
+print(pl3 + ggtitle("Histogram"))
+
+
+################################ Scatter plot #########################################################
+df <- mtcars
+
+#data & aesthetics layer
+
+pl <- ggplot(df,aes(x=wt,y=mpg))
+
+pl2 <- pl + geom_point(alpha=0.5,size = 5)
+#OR
+pl2 <- pl + geom_point(aes(shape = factor(cyl),color= factor(cyl)),size = 3)
+# using size for a discreate variable is not advised so we have to use "shape"
+#Or
+pl2 <- pl + geom_point(aes(color = hp),size = 3)
+# now we want to set low colour or high colour for plot
+pl3 <- pl2 + scale_color_gradient(low = 'blue',high = 'red')
+print(pl3)
+
+
+
+################################# Barplots ############################################################# 
+#barplots : shows relation between a numeric and a categorical variable
+df <- mpg
+
+pl <- ggplot(df , aes(x = class))
+print(pl+geom_bar(aes(fill = drv),position = 'dodge'))
+# position is for different stacking or diff bars ( usually used for compare groups)
+
+
+################################ Boxplots #################################################################
+# uses boxes and lines to depict the distributions of one or more groups of numeric data
+
+df <- mtcars
+
+pl <- ggplot(df,aes(x=factor(cyl),y=mpg))
+pl2 <- pl+geom_boxplot(aes(fill = factor(cyl))+theme_bw())
+print(pl2)
+## coord_flip() : flipping the coordinates
+
+######### 2 variable plotting
+library(ggplot2movies)
+
+pl <- ggplot(movies,aes(x = year,y=rating))
+pl2 <- pl+geom_bin2d()+ scale_fill_gradient(high = 'red',low = 'green')
+print(pl2)
+
+
+install.packages('hexbin')
+pl <- ggplot(movies,aes(x = year,y=rating))
+pl2 <- pl+geom_hex()
+print(pl2+ scale_fill_gradient(high = 'red',low='blue'))
+
+
+# 2d density plot
+pl2 <- pl + geom_density2d()
+print(pl2)
+
+
+### Cordinates and faceting
+
+pl <- ggplot(mpg,aes(x = displ,y = hwy))+geom_point()
+
+pl2 <- pl + coord_cartesian(xlim = c(1,4),ylim = c(15,30))
+
+# coord_fixed
+
+pl2 <- pl + coord_fixed(ratio = 1/3)
+
+print(pl2)
+
+
+#help('facet_grid')
+
+#facit grid
+print(pl + facet_grid(.~cyl))
+
+print(pl + facet_grid(drv ~cyl)) ## 2 dimensions of facet
+
+print(pl+facet_grid(drv ~.))
+
+
+#### Themes
+
+
+## applying a theme
+#theme_set(theme_minimal())
+pl <- ggplot(mtcars,aes(x = wt,y=mpg))+geom_point()
+print(pl +theme_dark())
+## 2 methods are defined above
 
